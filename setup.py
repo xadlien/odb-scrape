31e6cd6a53f021f0730b9442b285554a036d49f0
xadlien <djm24862@gmail.com>
2021-08-21 15:39:50 -0400

    added setup.py for building script

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='odb-scrape',
+      version='0.1',
+      description='Program to pull devotional info from Our Daily Bread',
+      url='https://github.com/xadlien/odb-scrape',
+      author='Daniel Martin',
+      author_email='djm24862@gmail.com',
+      packages=['odbscrape'],
+      entry_points = {
+          'console_scripts': ['odb-scrape=odbscrape.odb_scrape:main']
+      },
+      zip_safe=False)
